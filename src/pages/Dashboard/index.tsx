949e64b97dc5fdf7f56a16f37caee610f5c276ad
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2021-02-06 22:23:21 -0300

    created and styled next appointment

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
-import { FiPower } from 'react-icons/fi'
-import { Container, Header, HeaderContent, Profile } from './styles'
+import { FiClock, FiPower } from 'react-icons/fi'
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment
+} from './styles'
 
 import logoImg from '../../assets/logo.svg'
 import { useAuth } from '../../context/AuthContext'
@@ -25,6 +34,31 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src='http://localhost:3000/files/6de8a38073dd50872be1-profile.jpg'
+                alt='Mateus Tora'
+              />
+              <strong>Mateus Tora</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   )
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -57,4 +57,104 @@ const Profile = styled.div`
   }
 `
 
-export { Container, Header, HeaderContent, Profile }
+const Content = styled.main`
+  display: flex;
+  margin: 64px auto;
+  max-width: 1120px;
+`
+const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    align-items: center;
+    color: #ff9000;
+    display: flex;
+    font-weight: 400;
+    margin-top: 8px;
+  }
+
+  span {
+    align-items: center;
+    display: flex;
+  }
+
+  span + span::before {
+    background: #ff9000;
+    content: '';
+    height: 12px;
+    margin: 0 8px;
+    width: 1px;
+  }
+`
+const Calendar = styled.aside`
+  width: 380px;
+`
+
+const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  strong {
+    color: #999591;
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  div {
+    align-items: center;
+    background: #3e3b47;
+    border-radius: 10px;
+    display: flex;
+    margin-top: 24px;
+    padding: 16px 24px;
+    position: relative;
+
+    &::before {
+      background: #ff9000;
+      content: '';
+      height: 80%;
+      left: 0;
+      position: absolute;
+      top: 10%;
+      width: 1px;
+    }
+
+    img {
+      border-radius: 50%;
+      height: 80px;
+      width: 80px;
+    }
+
+    strong {
+      color: #fff;
+      margin-left: 24px;
+    }
+
+    span {
+      align-items: center;
+      color: #999591;
+      display: flex;
+      margin-left: auto;
+
+      svg {
+        color: #ff9000;
+        margin-right: 8px;
+      }
+    }
+  }
+`
+
+export {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment
+}
